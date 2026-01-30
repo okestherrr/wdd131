c60d568142109b89ef80a79bae9b7345eefbc4d9
okestherrr <okesthers@byui.edu>
2026-01-30 00:56:40 -0700

    i did so much it was a pain to fix

--- coolpics/index.js
+++ coolpics/index.js
@@ -1,27 +1,31 @@
 //modal image display
+
 const gallery = document.querySelector('.currentwork');
 const modal = document.querySelector('dialog');
 const modalImage = modal.querySelector('img');
 const closeButton = modal.querySelector('.close-viewer');
-
 gallery.addEventListener('click', openModal);
-
 function openModal(e) {
     const img = e.target;
     const src = img.getAttribute('src');
     const alt = img.getAttribute('alt');
-
-    modalImage.src = src;
-    modalImage.alt = alt;
-    modal.showModal();
+    
+    if (src) {
+        modalImage.setAttribute('src', src);
+        modalImage.setAttribute('alt', alt || '');
+        modal.showModal();
+    }
 }
-
 closeButton.addEventListener('click', () => {
     modal.close();
 });
-
 modal.addEventListener('click', (event) => {
     if (event.target === modal) {
         modal.close();
     }
+});
+const menuBtn = document.querySelector('.menu-btn');
+const nav = document.querySelector('nav');
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('show');
 });
